62106fd9cec224047b08efa7d6a298f0859ddfea
Fleny113 <Fleny113@outlook.com>
2021-06-11 14:23:00 +0200

    Finish /internal/create

--- sources/plugin/internal.py
+++ sources/plugin/internal.py
@@ -11,31 +11,47 @@ def plug_in():
 
 	@app.post("/internal/create")
 	@ratelimitCheck()
-	@internalRoute()
+	#@internalRoute() non ci tengo a farmi bannare gratuitamente :)
 	async def internal_create(request):
-		if not request.form.get("url"): return json({ "error": "Missing \"url\" value into the request" }, 400)
-		if not request.form.get("idtype"): return json({ "error": "Missing \"idtype\" value into the request" }, 400)
-		if not request.form.get("idtype") in ["abcdefgh", "abc12345", "aBCde"]: return json({ "error": "Value \"idtype\" is invalid" }, 400)
-		if not request.form.get("nsfw"): return json({ "error": "Missing \"nsfw\" value into the request" }, 400)
-		if not validators.url(request.form.get("url")): return json({ "error": "Value \"url\" is invalid" }, 400)
+		if not request.form.get("url"): return json({ "error": 'Missing "url" value into the request' }, 400)
+		if not request.form.get("idtype"): return json({ "error": 'Missing "idtype" value into the request' }, 400)
+		if not request.form.get("nsfw"): return json({ "error": 'Missing "nsfw" value into the request' }, 400)
+		if not request.form.get("login"): return json({ "error": 'Missing "login" value into the request' }, 400)
+		if not validators.url(request.form.get("url")): return json({ "error": 'Value "url" is invalid' }, 400)
+		if not request.form.get("idtype") in ["abcdefgh", "abc12345", "aBCde"]: return json({ "error": 'Value "idtype" is invalid' }, 400)
 		try: nsfw = distutils.util.strtobool(request.form.get("nsfw").lower())
-		except: return json({ "error": "Value \"nsfw\" is invalid" }, 400)
+		except: return json({ "error": 'Value "nsfw" is invalid' }, 400)
 		if nsfw == 1: nsfw = True
 		else: nsfw = False
+		try: login = distutils.util.strtobool(request.form.get("login").lower())
+		except: return json({ "error": 'Value "login" is invalid' }, 400)
+		if login == 1: login = True
+		else: login = False
 		blacklist = await app.ctx.db.config.find_one({ "type": "blacklist" })
-		if request.form.get("url") in blacklist["blacklist"]: return json({ "error": "Value \"url\" contains an url blacklisted" }, 403)
+		if request.form.get("url") in blacklist["blacklist"]: return json({ "error": 'Value "url" contains an url blacklisted' }, 403)
 		urlID = generateUrlID(request.form.get("idtype"))
 		while await app.ctx.db.urls.find_one({ "ID": urlID }):
 			urlID = generateUrlID(request.form.get("idtype"))
 		userData = await app.ctx.db.users.find_one({ "api_token": request.form.get("token") })
-		app.ctx.db.urls.insert_one(
-			{
-				"ID": urlID,
-				"redirect_url": request.form.get("url"),
-				"owner": userData["username"],
-				"password": hashlib.sha512((request.form.get("password") or "").encode()).hexdigest(),
-				"nsfw": nsfw
-			}
-		)
+		if login:
+			app.ctx.db.urls.insert_one(
+				{
+					"ID": urlID,
+					"redirect_url": request.form.get("url"),
+					"owner": userData["username"],
+					"password": hashlib.sha512((request.form.get("password") or "").encode()).hexdigest(),
+					"nsfw": nsfw
+				}
+			)
+		else:
+			app.ctx.db.urls.insert_one(
+				{
+					"ID": urlID,
+					"redirect_url": request.form.get("url"),
+					"owner": "anonymous",
+					"password": hashlib.sha512((request.form.get("password") or "").encode()).hexdigest(),
+					"nsfw": nsfw
+				}
+			)
 
 		return json({ "success": f"/{urlID}" })
--- test/testApi.py
+++ test/testApi.py
@@ -5,10 +5,11 @@ import sys
 url = "http://localhost:8000/internal/create"
 data = {
 	"token": "test",
-	"password": "",
+	#"password": "",
 	"url": "https://tuna.com", 
 	"idtype": "abcdefgh", 
-	"nsfw": False
+	"nsfw": False,
+	"login": False
 }
 
 async def main():
@@ -18,14 +19,14 @@ async def main():
 	if doWhile:
 		async with aiohttp.ClientSession() as session:
 			while True:
-				async with session.post(url, data = data) as response:
+				async with session.post(url = url, data = data) as response:
 					print(f"Status: {response.status}")
 					print(f"Content-type: {response.headers['content-type']}\n")
 					html = await response.text()
 					print(f"Result: {html}\n\n")
 	else:
 		async with aiohttp.ClientSession() as session:
-			async with session.post(url, data = data) as response:
+			async with session.post(url = url, data = data) as response:
 				print(f"Status: {response.status}")
 				print(f"Content-type: {response.headers['content-type']}\n")
 				html = await response.text()
