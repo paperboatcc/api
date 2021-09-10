218e1013cfdaf3cbd6ce1a8b9cae0ba36becffa8
Fleny113 <Fleny113@outlook.com>
2021-09-10 12:52:23 +0200

    removed ratelimit.json

--- sources/plugin/assets.py
+++
@@ -1,8 +0,0 @@
-from sanic import Sanic
-from sanic.response import file
-
-def plug_in():
-	app = Sanic.get_app("api.fasmga")
-
-	@app.get("/favicon.ico")
-	async def favicon(request): return await file("assets/favicon.ico")
--- sources/plugin/docs.py
+++
@@ -1,8 +0,0 @@
-from sanic import Sanic
-from sources.utities import render_template
-
-def plug_in():
-	app = Sanic.get_app("api.fasmga")
-
-	@app.get("/")
-	async def main(request): return render_template("main.html")
