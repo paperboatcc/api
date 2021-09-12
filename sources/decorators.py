7f38196be0727578e6d5001bcd286d846cad3a2e
ParliamoDiPC <66361695+ParliamoDiPC@users.noreply.github.com>
2021-09-12 12:30:17 +0200

    changing a little thing on the ip getting system

--- sources/decorators.py
+++ sources/decorators.py
@@ -18,7 +18,7 @@ def ratelimitCheck():
 				return json({ "bad_request": "Token you provvided is invalid" }, 400)
 			user = userData["username"]
 			if (userData["is_banned"] == True): 
-				logger.warning(f"{userData['username']} e bannato e sta tentando di accedere ({request.ip})")
+				logger.warning(f"{userData['username']} e bannato e sta tentando di accedere ({request.headers.get("HTTP_X_FORWARDED_FOR")})")
 				return json({ "banned": "You are banned from api, you can try to contact fasmga staff to get unban" }, 401)
 
 			jsonFile = open("sources/ratelimit.json", "r")
@@ -26,25 +26,25 @@ def ratelimitCheck():
 			jsonFile.close()
 			if user == 'anonymous':
 				if not 'anonymous' in jsonValue: jsonValue['anonymous'] = {}
-				if not request.ip in jsonValue['anonymous']: jsonValue['anonymous'][request.ip] = 0
-				if jsonValue['anonymous'][request.ip] <= 20:
-					jsonValue['anonymous'][request.ip] += 1
+				if not request.headers.get("HTTP_X_FORWARDED_FOR") in jsonValue['anonymous']: jsonValue['anonymous'][request.headers.get("HTTP_X_FORWARDED_FOR")] = 0
+				if jsonValue['anonymous'][request.headers.get("HTTP_X_FORWARDED_FOR")] <= 20:
+					jsonValue['anonymous'][request.headers.get("HTTP_X_FORWARDED_FOR")] += 1
 					jsonw = open("sources/ratelimit.json", "w")
 					jsonModule.dump(jsonValue, jsonw, indent = 2, sort_keys = True)
 					jsonw.close()
 					response = await f(request, *args, **kwargs)
 					return response
 				else:
-					jsonValue['anonymous'][request.ip] += 1
+					jsonValue['anonymous'][request.headers.get("HTTP_X_FORWARDED_FOR")] += 1
 					jsonw = open("sources/ratelimit.json", "w")
 					jsonModule.dump(jsonValue, jsonw, indent = 2, sort_keys = True)
 					jsonw.close()
-					if (jsonValue['anonymous'][request.ip] >= 50):
-						if (jsonValue['anonymous'][request.ip] == 50):
-							app.ctx.webhook.post(content = f"⚠️ | Warning, {request.ip} done 50 request in less than a minute") 
-						if (jsonValue['anonymous'][request.ip] == 100):
-							app.ctx.webhook.post(content = f"ℹ️ | Just for information, I can't ban {request.ip} but he / she did 100 requests in less than a minute")
-							logger.warning(f"{request.ip} e bannato e sta tentando di accedere")
+					if (jsonValue['anonymous'][request.headers.get("HTTP_X_FORWARDED_FOR")] >= 50):
+						if (jsonValue['anonymous'][request.headers.get("HTTP_X_FORWARDED_FOR")] == 50):
+							app.ctx.webhook.post(content = f"⚠️ | Warning, {request.headers.get("HTTP_X_FORWARDED_FOR")} done 50 request in less than a minute") 
+						if (jsonValue['anonymous'][request.headers.get("HTTP_X_FORWARDED_FOR")] == 100):
+							app.ctx.webhook.post(content = f"ℹ️ | Just for information, I can't ban {request.headers.get("HTTP_X_FORWARDED_FOR")} but he / she did 100 requests in less than a minute")
+							logger.warning(f"{request.headers.get("HTTP_X_FORWARDED_FOR")} e bannato e sta tentando di accedere")
 							return json({ "SOTP": "okey, but now STOP! then i obbligated to ban you, you can try to contact fasmga staff to get unban" }, 401)
 						return json({ "ddos": "Are you trying to DDoS our API? Asking for a friend :)" }, 429)
 					return json({ "ratelimit": "You did >20 requests to the API in this minute. Wait a minute and try again." }, 429)
@@ -68,8 +68,8 @@ def ratelimitCheck():
 						if (jsonValue[user] == 100):
 							await app.ctx.db.users.find_one_and_update({ "api_token": request.form.get("token") }, { "$set": { "is_banned": True }})
 							app.ctx.webhook.post(content = f"ℹ️ | Just for information, I banned {user} because it did 100 requests in less than a minute")
-							logger.warning(f"{user} e bannato e sta tentando di accedere ({request.ip})")
-							return json({ "SOTP": "okey, but now STOP! then i obbligated to ban you, you can try to contact fasmga staff to get unban" }, 401)
+							logger.warning(f"{user} e bannato e sta tentando di accedere ({request.headers.get("HTTP_X_FORWARDED_FOR")})")
+							return json({ "STOP": "okey, but now SOTP! then i obbligated to ban you, you can try to contact fasmga staff to get unban" }, 401)
 						return json({ "ddos": "Are you trying to DDoS our API? Asking for a friend :)" }, 429)
 					return json({ "ratelimit": "You did >20 requests to the API in this minute. Wait a minute and try again." }, 429)
 		return decorated_function
@@ -84,13 +84,13 @@ def internalRoute():
 				return json({ "bad_request": "Token you provvided is invalid" }, 400)
 			user = userData['username']
 			if userData["is_banned"]:
-				logger.warning(f"{user} e bannato e sta tentando di accedere ({request.ip})")
+				logger.warning(f"{user} e bannato e sta tentando di accedere ({request.headers.get("HTTP_X_FORWARDED_FOR")})")
 				return json({ "banned": "You are banned from api, you can try to contact fasmga staff to get unban" }, 401)
-			if not request.ip == "127.0.0.1":
+			if not request.headers.get("HTTP_X_FORWARDED_FOR") == "127.0.0.1":
 				await app.ctx.db.users.find_one_and_update({ "api_token": request.form.get("token") }, { "$set": { "is_banned": True }})
 				app.add_task(tempBanRemove(request.form.get("token")))
 				app.ctx.webhook.post(content = f"⚠️ | Warning, {user} is trying to access to internal APIs", username = "Fasm.ga Iternal")
-				logger.warning(f"{userData['username']} e stato temp bannato ({request.ip})")
+				logger.warning(f"{userData['username']} e stato temp bannato ({request.headers.get("HTTP_X_FORWARDED_FOR")})")
 				return json({ "unauthorized": "You can't access to internal APIs, use normal APIs instead, for security reason you are temp-banned for 10 minutes" }, 401)
 			response = await f(request, *args, **kwargs)
 			return response
