15cd68d4bd26776996c9c719cab8eb8d62c957fc
Fleny113 <Fleny113@outlook.com>
2021-11-15 21:19:25 +0100

    Cleaned working directory from python files and readme update
    - Updated README.md
    - Updated ignore list for .gitignore file
    
    - Removed python files

--- sources/plugin/docs.py
+++
@@ -1,7 +0,0 @@
-from sanic import Sanic
-from sources.utility import render_template
-
-app = Sanic.get_app("api.fasmga")
-
-@app.get("/")
-async def main(request): return render_template("main.html", 200)
